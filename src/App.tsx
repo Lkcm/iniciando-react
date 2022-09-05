fce615930cedef27cf6f2bdda209d8bc082e7ff3
Lucas Kroger <lucasmkroger12@gmail.com>
2022-09-04 23:27:08 -0300

    iniciando-react com vue e typeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./components/App.css"
+import { AppRoutes } from "./Routes"
+
+function App() {
+//---------- EXEMPLO COM O ROUTES
+
+return (
+  <AppRoutes/>
+  
+);
+}
+
+export default App
